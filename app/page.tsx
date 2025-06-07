73578d82b66f4a757e824fc2669305b92e3ffbec
AymenBraikia <aymenbraikia1@gmail.com>
2025-06-07 14:53:27 +0100

    x

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<>
 			<Header />
-			<div className="mainContainer" style={{ position: "absolute", top: "60px", left: "0", height: "fit-content", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", rowGap: 50, padding: "0 100px" }}>
+			<div className="mainContainer" style={{ position: "absolute", top: "60px", left: "0", height: "fit-content", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", rowGap: 100, padding: "0 100px" }}>
 				<Introduction />
 				<Projects />
 				<About />
